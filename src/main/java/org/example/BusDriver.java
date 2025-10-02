2bcbef98fb97226e1dac919ed95acbd50b097fbf
Diego Machín <diego@acidtango.com>
2025-10-02 16:23:32 +0100

    Green: create clases

---
+++ src/main/java/org/example/BusDriver.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.List;
+
+public class BusDriver {
+    private final List<Integer> stops;
+
+    public BusDriver(List<Integer> stops) {
+        this.stops = stops;
+    }
+}
---
+++ src/main/java/org/example/BusGossipSystem.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.List;
+
+public class BusGossipSystem {
+    private final List<BusDriver> drivers;
+
+    public BusGossipSystem(List<BusDriver> drivers) {
+        this.drivers = drivers;
+    }
+
+    public int simulate() {
+        return 5;
+    }
+}
