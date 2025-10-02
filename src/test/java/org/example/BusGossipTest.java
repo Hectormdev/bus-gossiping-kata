cb4957cc9a2a5d0dd0c3a13d4efbbe5910d98026
Diego Machín <diego@acidtango.com>
2025-10-02 16:25:49 +0100

    Red: not calculating gossiping

--- src/test/java/org/example/BusGossipTest.java
+++ src/test/java/org/example/BusGossipTest.java
@@ -1,20 +1,33 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BusGossipTest {
 
-  @Test
-  void itSharesGossipAt5Iteration(){
-    var firstDriver = new BusDriver(List.of(3,1,2,3));
-    var secondDriver = new BusDriver(List.of(3,2,3,1));
-    var thirdDriver = new BusDriver(List.of(4,2,3,4,5));
-    var gossipSystem = new BusGossipSystem(List.of(firstDriver, secondDriver, thirdDriver));
-    int result = gossipSystem.simulate();
-    assertEquals(5, result);
-  }
+    @Test
+    void itSharesGossipAt5Iteration() {
+        var firstDriver = new BusDriver(List.of(3, 1, 2, 3));
+        var secondDriver = new BusDriver(List.of(3, 2, 3, 1));
+        var thirdDriver = new BusDriver(List.of(4, 2, 3, 4, 5));
+        var gossipSystem = new BusGossipSystem(List.of(firstDriver, secondDriver, thirdDriver));
+        int result = gossipSystem.simulate();
+        assertEquals(5, result);
+    }
+
+    @Test
+    void itStopsOnAllSharedSecrets() {
+        var firstDriver = new BusDriver(List.of(5, 2, 8));
+        var secondDriver = new BusDriver(List.of(5, 2, 8));
+        var thirdDriver = new BusDriver(List.of(5, 2, 8));
+        var fourthDriver = new BusDriver(List.of(5, 2, 8));
+        var fifthDriver = new BusDriver(List.of(5, 2, 8));
+
+        var gossipSystem = new BusGossipSystem(List.of(firstDriver, secondDriver, thirdDriver, fourthDriver, fifthDriver));
+        int result = gossipSystem.simulate();
+        assertEquals(1, result);
+    }
 }
